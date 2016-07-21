803749dce08349e5a2529c0a2b0348c0526b370d
Katie Ho <kho@umass.edu>
2016-07-21 12:51:03 -0700

    all factorial tests pass!

--- src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
@@ -2,6 +2,14 @@ package com.thoughtworks.tddintro.factorial;
 
 public class Factorial {
     public Integer compute(int i) {
-        return -1;
+        if(i<0)
+            throw new IllegalArgumentException("Invalid input: negative.");
+        else {
+            if (i == 0)
+                return 1;
+            else
+                return compute(i - 1) * i;
+        }
     }
+
 }
